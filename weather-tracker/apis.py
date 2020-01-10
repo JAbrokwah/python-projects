a97aee3c11f34ab4728f8d14d37bbbce7c52283f
Justin Abrokwah <justin.abrokwah@mail.utoronto.ca>
2020-01-10 16:03:06 -0500

    Revamped Weather tool with Weather Unlocked API instead of Amdoren! Implemented feature for user to output forecast to a text file.

--- weather-tracker/apis.py
+++ weather-tracker/apis.py
@@ -1,6 +1,9 @@
+# Geolocation API credentials from IPGeolocation API
 location_key = "538f5061afde419c878a36319676ef48"
 location_url = "https://api.ipgeolocation.io/ipgeo?apiKey={}&fields=geo".format(location_key)
 
-weather_key = "wdgTxwgvB3TcKSzdHtJU8U5a2zyFUz"
-base_weather_url = "https://www.amdoren.com/api/weather.php?api_key={}".format(weather_key)  # &lat={}&lon={} for weather
-# Powered by <a href="https://www.amdoren.com">Amdoren</a>
+# Weather API credentials from Weather Unlocked API
+weather_app_id = "2024759e"
+weather_app_key = "3f3625c5dc2896a72aaa5e211d1161fb"
+base_weather_url = "http://api.weatherunlocked.com/api/forecast/lat,long?app_id=appid&app_key=appkey"
+
--- weather-tracker/main.py
+++ weather-tracker/main.py
@@ -1,12 +1,37 @@
-from weather_tracker import weather_tracker
+from weather_tracker import weather_tracker, output_file_path
 from wt_exceptions import WeatherException, LocationException
 
+
+def check_selection_value(value):
+    return value in [1, 2]
+
+
 if __name__ == '__main__':
-    print("Welcome to the Automatic Weather Machine! We track you and tell you the weather for the week")
+    print("Welcome to the Automatic Weather Machine! We find your location (Based on IP Address) and tell you the "
+          "weather for your area for the week")
+
+    output = None
 
     try:
-        forecast, city, district = weather_tracker()
-        print("Here is the forecast for {}, {}:".format(city, district))
-        print(forecast)
+        print('1 - Print to Console')
+        print('2 - Output to TXT File')
+        while output is None or type(output) != int or output > 2 or output < 1:
+            try:
+                output = int(input("Output Selection: "))
+                if not check_selection_value(output):
+                    output = None
+                    print("Provide a valid selection for output!")
+            except ValueError:
+                print("{} is not a number, please enter a number only".format(output))
+        result = weather_tracker(output)
+        if len(result) != 3:
+            print('You can find your forecast in the file: {}'.format(output_file_path))
+        else:
+            forecast = result[0]
+            city = result[1]
+            district = result[2]
+            print("Here is the forecast for {}, {}:".format(city, district))
+            for day in forecast:
+                print(day)
     except (LocationException, WeatherException) as error:
         print(error.args[0])
